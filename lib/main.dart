ac592ecbe3ff9148f6afd5a6059437e1b29f4739
lizhiming <lizhiming@ksjgs.com>
2019-11-22 11:39:51 +0800

    更改标题

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Daily End',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Daily Home'),
     );
   }
 }
